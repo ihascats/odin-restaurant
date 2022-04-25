9f04dbdcf229ef87ff7af3c8ceb98ae2c834546e
Stefan <s.pavlovic.rs@gmail.com>
2022-04-25 22:30:39 +0200

    Add basic file structure

---
+++ src/index.js
@@ -0,0 +1,3 @@
+import test from "./js/scripts";
+
+console.log(test());
---
+++ src/js/scripts.js
@@ -0,0 +1,5 @@
+function test(){
+    return ("It's worki~ng! I've been imported from scripts.js!");
+}
+
+export default test;
