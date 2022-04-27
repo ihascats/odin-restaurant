212174a3f7cc3806d28cbb591800adcabc6e1ec8
Stefan <s.pavlovic.rs@gmail.com>
2022-04-27 20:03:49 +0200

    Dynamically change page content

--- src/index.js
+++ src/index.js
@@ -3,10 +3,24 @@ import layout from "./js/permLayout";
 import menuPage from "./js/menu";
 
 const container = document.querySelector('.container');
-
 layout(container);
+const pageItems = document.querySelector('.page-content');
+
+let ul = document.querySelector('ul');
+ul.addEventListener('click', function(event){
+    let li = event.target
+    if (!li || li == null) return;
+    while (pageItems.firstChild) {
+        pageItems.removeChild(pageItems.firstChild);
+    }
+    if (li.textContent.toLowerCase() == 'home'){
+        homePage(document.querySelector('.container'));
+    }
+    if (li.textContent.toLowerCase() == 'menu'){
+        menuPage(document.querySelector('.page-content'));
+        require("./css/menu.css");
+    }
+
+});
 
 console.log(test());
-require("./css/menu.css");
-// homePage(document.querySelector('.container'));
-menuPage(document.querySelector('.page-content'))
